a968e75c2af6609ab1413cf5e0d899b14093a27c
Mudit Sharma <muditshar001@gmail.com>
2023-05-17 19:38:59 +0530

    Update taskMiddleware.js

--- middleware/taskMiddleware.js
+++ middleware/taskMiddleware.js
@@ -1,62 +1,35 @@
-const Tasks   = require("../models/task.js");
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = "newtonSchool";
+const jwt = require('jsonwebtoken');
+const Task = require('../models/task');
 
-/*
-
-req.body = {
-    "task_id"    : task_id,
-    "token"      : token
-}
-
-Response
-
-1. Unabel to verify token (Invalid Token)
-404 Status Code
-json = 
-{
-    status: 'fail',
-    message: 'Invalid token'
-}
-
-2. if Given task_id does not exist in Tasks 
-
-404 Status code
-
-json = {
-    status: 'fail',
-    message: 'Given task doesnot exist'
-}
-
-3. if creator_id of task that belong to task_id is not same as userId that we get from payload of token
-   means this is not the owner of given task hence
-
-403 Status code
-json = 
-{
-    status: 'fail',
-    message: 'Access Denied'
-}
-
-4. if creator_id of task that belong to task_id is same as userId that we get from payload of token
-   means this is the owner of given task hence
-
-200 Status code with allowing further.
-
-*/
-
-async function isowner(req, res, next) {
-
-    try {
-        
-        //Write your code here.
-
-    } catch (err) {
-        return res.status(400).json({
-            status: "error",
-            message: "Unable to check"
-        })
+const isOwner = async (req, res, next) => {
+  try {
+    const token = req.body.token;
+    if (!token) {
+      return res.status(404).json({ message: 'Invalid token', status: 'fail' });
     }
-}
 
-module.exports = { isowner };
+    // Verify the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(404).json({ message: 'Invalid token', status: 'fail' });
+    }
+
+    const task = await Task.findById(req.body.task_id);
+    if (!task) {
+      return res.status(404).json({ message: 'Given task does not exist', status: 'fail' });
+    }
+
+    if (task.creator_id !== decoded.userId) {
+      return res.status(403).json({ message: 'Access Denied', status: 'fail' });
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: error.message, status: 'fail' });
+  }
+};
+
+module.exports = {
+  isOwner
+};
